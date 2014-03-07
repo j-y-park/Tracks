1bf5936fff2f2268911f814e70b363cf03b27a60
JulianJaffe <julianjaffe@berkeley.edu>
2014-03-07 13:35:11 -0800

    User Models
    User models; could still use some work. I've noticed a flaw in the
    views, so this will be up shortly.

--- Tracks/Tracks/settings.py
+++ Tracks/Tracks/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'TracksUser',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -77,5 +78,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'TracksUser.TracksUser'
 
 from localsettings import *
--- Tracks/TracksUser/models.py
+++ Tracks/TracksUser/models.py
@@ -1,7 +1,56 @@
 from django.db import models
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager)
 
-# Create your models here.
+class TracksUserManager(BaseUserManager):
+    """A class to manage user creation. May not in fact be necessary."""
+    def create_user(self, email, firstName, lastName, password=None):
+        if get_by_natural_key(email): #email already exists
+            raise ValueError('User with that email address already exists')
+        if password == None: #Add other strength checks later
+            raise ValueError('Bad password!') #perhaps we should use error codes instead?
+        user = self.model(
+            email=self.normalize_email(email),
+            firstName=firstName,
+            lastName=lastName
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_super_user(self, email, password):
+        """For now, fail"""
+        raise
 
 class TracksUser(AbstractBaseUser):
+    """
+    The class for Tracks Users. As of now requires an email address,
+    a first name, and a last name. 
+    """
     email = models.EmailField(max_length=254, unique=True)
+    firstName = models.CharField(max_length=45)
+    lastName = models.CharField(max_length=45)
+    #add support for middle names?
+    #require a birthday (i.e. minimum age)?
+    #store joined date?
+    is_active = models.BooleanField(default=True)
+
+    objects = TracksUserManager()
+    
+    REQUIRED_FIELDS = ['firstName', 'lastName']
     USERNAME_FIELD = 'email'
+
+    def __unicode__(self):
+        """Return the username (i.e. email address)"""
+        return self.email
+
+    def get_full_name(self):
+        """Returns the user's full name."""
+        fullName = self.firstName.strip() + " " + self.lastName.strip()
+        return firstName
+
+    def get_short_name(self):
+        """Returns the user's first name."""
+        return self.firstName.strip()
+        
+
